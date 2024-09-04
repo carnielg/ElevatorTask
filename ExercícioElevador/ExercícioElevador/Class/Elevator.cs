ba47aa6fc106ad2c4ee0d6a5cfee5c0a0f68d141
carnielg <guicarniell22@gmail.com>
2024-09-03 22:03:36 -0300

    ElevatorTask

---
+++ ExercícioElevador/ExercícioElevador/Class/Elevator.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercícioElevador.Class
+{
+    class Elevator
+    {
+
+        public int Capacidade { get; set; }
+        public int Andar { get; set; }
+        public int TotalAndar { get; set; }
+        public int Pessoas { get; set; }
+
+
+        public Elevator()
+        {
+            Andar = 0;
+            Pessoas = 0;
+        }
+
+        public void Incializar(int totalandares, int capacidade)
+        {
+            this.Capacidade = capacidade;
+            this.TotalAndar = totalandares;
+      
+        }
+        public string Entrar()
+        {
+            if (Pessoas >= Capacidade)
+            {
+                return
+                    "A capacidade do elevador foi excedida!\n";
+            }
+            else
+            {
+                Pessoas++;
+            }
+            return
+                $"Há {Pessoas} pessoas no elevador.\n";
+
+        }
+        public string Sair()
+        {
+            if (Pessoas <= 0)
+            {
+                return
+                    "Não há pessoas no elevador.\n";
+            }
+            else
+            {
+                Pessoas--;
+                return
+                  $"Há {Pessoas} pessoas no elevador.\n";
+            }
+
+        }
+        public int Valor { get; set; }
+        public string Subir()//int valor determina o andar que a pessoa quer ir
+        {
+            if (Valor > TotalAndar)
+            {
+                return
+                    $"Só é possível subir até o andar {Valor}.\n";
+            }
+            else
+            {
+                if (Andar >= TotalAndar)
+                {
+                    return
+                        $"O elevador está no último andar.\n";
+                }
+                else
+                {
+                    if (Valor > Andar)
+                    {
+                        Andar = Valor;
+                        return
+                            $"Você está no andar {Andar}\n";
+                    }
+                    else
+                    {
+                        return
+                        "Só é possível subir.\n";
+                    }
+                }
+            }
+        }
+        public string Descer()
+        {
+            if (Valor < 0)
+            {
+                return
+                    "Só é possível ir até o andar 0\n";
+            }
+            else 
+            {
+                if (Andar <= 0)
+                {
+                    return
+                        $"O elevador está no térreo.\n";
+                }
+                else
+                {
+                    if (Valor < Andar)
+                    {
+                        Andar = Valor;
+                        return
+                            $"Você está no andar {Andar}\n";
+                    }
+                    else
+                    {
+                        return
+                        "Só é possível descer.\n";
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ ExercícioElevador/ExercícioElevador/Program.cs
@@ -0,0 +1,145 @@
+﻿using ExercícioElevador.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ExercícioElevador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevator Elevador1 = new Elevator();
+
+            int quit = 0;
+
+            Console.WriteLine("Qual a capacidade máxima de pessoas no elevador?");
+            Elevador1.Capacidade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantos andares existem?");
+            Elevador1.TotalAndar = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Iniciando Elevador...");
+            // Define o tempo total de carregamento em segundos
+
+            int totalTimeInSeconds = 3;
+            // Define o intervalo de atualização da barra em milissegundos
+
+            int updateIntervalInMilliseconds = 100; // Atualiza a cada 100 ms
+
+            // Calcula o número total de atualizações
+            int totalUpdates = totalTimeInSeconds * 1000 / updateIntervalInMilliseconds;
+
+            for (int i = 0; i <= totalUpdates; i++)
+            {
+                // Calcula a porcentagem concluída
+                double progress = (i / (double)totalUpdates) * 100;
+
+                // Cria a barra de carregamento
+                string progressBar = new string('#', i * 50 / totalUpdates) + new string('-', 50 - i * 50 / totalUpdates);
+
+                // Move o cursor para o início da linha
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write($"[{progressBar}] {progress:0.00}%");
+
+                // Aguarda o próximo intervalo de atualização
+                Thread.Sleep(updateIntervalInMilliseconds);
+            }
+            // Adiciona uma nova linha após a barra de carregamento
+
+            Console.WriteLine();
+            Console.WriteLine("Carregamento concluído!");
+            int n_case;
+            Console.Clear();
+            while (quit != 5)
+            {
+                Console.WriteLine($"|1 - Entrar\n|2 - Sair\n|3 - Subir\n|4 - Descer\n|5 - Fechar Menu");
+                n_case = int.Parse(Console.ReadLine());
+                quit = n_case;
+                if (n_case > 0 && n_case <= 5)
+                {
+                    switch (n_case)
+                    {
+                        case 1:
+                            Console.Write(Elevador1.Entrar());
+
+                            break;
+                        case 2:
+                            Console.Write(Elevador1.Sair());
+
+                            break;
+                        case 3:
+                            if (Elevador1.Valor == Elevador1.TotalAndar)
+                            {
+                                Console.WriteLine("Você já está no último andar.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Deseja subir para qual andar?");
+                                Elevador1.Valor = int.Parse(Console.ReadLine());
+                                Console.Write(Elevador1.Subir());
+                            }
+                            break;
+                        case 4:
+                            if (Elevador1.Valor == 0)
+                            {
+                                Console.WriteLine("Você já está no térreo.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Deseja descer para qual andar?");
+                                Elevador1.Valor = int.Parse(Console.ReadLine());
+                                Console.Write(Elevador1.Descer());
+                            }
+                            break;
+
+                    }
+                    Console.WriteLine("Aperte uma tecla para continuar...");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Não há essa opção, selecione novamente!");
+                    Console.WriteLine("Aperte uma tecla para continuar...");
+                    Console.ReadKey();
+                }
+                Console.Clear();
+            }
+            Console.WriteLine("Encerrando Elevador...");
+            // Define o tempo total de carregamento em segundos
+
+            totalTimeInSeconds = 3;
+            // Define o intervalo de atualização da barra em milissegundos
+
+            updateIntervalInMilliseconds = 100; // Atualiza a cada 100 ms
+
+            // Calcula o número total de atualizações
+            totalUpdates = totalTimeInSeconds * 1000 / updateIntervalInMilliseconds;
+
+            for (int i = 0; i <= totalUpdates; i++)
+            {
+                // Calcula a porcentagem concluída
+                double progress = (i / (double)totalUpdates) * 100;
+
+                // Cria a barra de carregamento
+                string progressBar = new string('#', i * 50 / totalUpdates) + new string('-', 50 - i * 50 / totalUpdates);
+
+                // Move o cursor para o início da linha
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write($"[{progressBar}] {progress:0.00}%");
+
+                // Aguarda o próximo intervalo de atualização
+                Thread.Sleep(updateIntervalInMilliseconds);
+            }
+            // Adiciona uma nova linha após a barra de carregamento
+
+            Console.WriteLine();
+            Console.WriteLine("Encerrado com Sucesso!");
+            Console.ReadKey();
+
+        }
+    }
+}
